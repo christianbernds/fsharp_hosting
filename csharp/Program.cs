57b5260990e9d02271be521c037952d4db2a416f
ChristianBernds <Christian.Bernds@5minds.de>
2021-01-21 17:11:02 +0100

    Integrate AtlasEngine in both projects

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,6 +1,12 @@
 namespace CSharp.WebApi
 {
+    using AtlasEngine;
+    using AtlasEngine.ApiClient;
+
+    using CSharp.WebApi.ExternalTaskHandler;
+
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
 
     public class Program
@@ -12,6 +18,17 @@ namespace CSharp.WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .UseExternalTaskWorkers(opt => opt.ActivateHandlersInScopes = true)
+                .ConfigureServices((context, services) =>
+                {
+                    services.Configure<ApiClientSettings>(context.Configuration.GetSection("ExternalTaskWorker"));
+
+                    services.AddScoped<AddHandler>();
+                })
+                .ConfigureWebHostDefaults(
+                    webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
     }
 }
--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -1,6 +1,10 @@
 namespace FSharp.WebApi
 
+open AtlasEngine
+open AtlasEngine.ApiClient
+open FSharp.WebApi.ExternalTaskHandler
 open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 
 module Program =
@@ -8,6 +12,10 @@ module Program =
 
     let CreateHostBuilder args =
         Host.CreateDefaultBuilder(args)
+            .UseExternalTaskWorkers(fun options -> options.ActivateHandlersInScopes = true |> ignore)
+            .ConfigureServices(fun context services ->
+                services.Configure<ApiClientSettings>(context.Configuration.GetSection("ExternalTaskWorker")) |> ignore
+                services.AddScoped<AddHandler>() |> ignore)
             .ConfigureWebHostDefaults(fun webBuilder ->
                 webBuilder.UseStartup<Startup>() |> ignore
             )
