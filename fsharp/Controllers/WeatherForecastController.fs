cbbb89acfc8f490197d6256f0dc91ac81508e869
ChristianBernds <Christian.Bernds@5minds.de>
2021-01-21 16:21:23 +0100

    Add fsharp project

---
+++ fsharp/Controllers/WeatherForecastController.fs
@@ -0,0 +1,35 @@
+﻿namespace FSharp.WebApi.Controllers
+
+open System
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open FSharp.WebApi
+
+[<ApiController>]
+[<Route("[controller]")>]
+type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
+    inherit ControllerBase()
+
+    let summaries =
+        [|
+            "Freezing"
+            "Bracing"
+            "Chilly"
+            "Cool"
+            "Mild"
+            "Warm"
+            "Balmy"
+            "Hot"
+            "Sweltering"
+            "Scorching"
+        |]
+
+    [<HttpGet>]
+    member _.Get() =
+        let rng = System.Random()
+        [|
+            for index in 0..4 ->
+                { Date = DateTime.Now.AddDays(float index)
+                  TemperatureC = rng.Next(-20,55)
+                  Summary = summaries.[rng.Next(summaries.Length)] }
+        |]
